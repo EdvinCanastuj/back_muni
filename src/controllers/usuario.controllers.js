8b68c93dcb011bb81914d9e4d276bed338d794f4
EdvinCanastuj <ecanastujv@miumg.edu.gt>
2023-10-06 18:50:22 -0600

    usuario.controllers funcional

--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -1,4 +1,5 @@
 import {getConnection} from "./../db/database";
+//api para consultar todos los usuarios
 const getUsuarios= async (req, res)=> {
     try{
         const connection= await getConnection();
@@ -9,57 +10,63 @@ const getUsuarios= async (req, res)=> {
         res.send(error.message);
     }
 };
+//api para consultar un usuario
 const getUsuario= async (req, res)=> {
     try{
         console.log(req.params);
         const {id_usuario} = req.params;
         const connection= await getConnection();
-        const result = await connection.query("SELECT * FROM usuario WHERE Id_usuario = ?;",id_usuario);      
+        const result = await connection.query("SELECT * FROM usuario WHERE id_usuario = ?;",id_usuario);      
         res.json(result);
     }catch(error){
         res.status(500);
         res.send(error.message);
     }
 };
+//api para agregar un usuario
 const addUsuario = async (req, res)=> {
     try{
         //cambiarlo por los campos de la tabla
-        const { id_usuario, nombre_usuario, nombre, apellido, contraseña, avarage } = req.body;
-        if(id == undefined || name==undefined || lastname==undefined || brithdate==undefined || career==undefined || avarage==undefined){
+        const { nombre_usuario, nombre, apellido, contrasena, codigo, id_rol, id_cargo, id_dependencia } = req.body;
+        if( nombre_usuario==undefined || nombre==undefined || apellido==undefined || contrasena==undefined || codigo==undefined || id_rol==undefined || id_cargo==undefined || id_dependencia==undefined){
             res.status(400).json({message:"Bad Request. Please fill all field."});
         }
-        const students = { id, name, lastname, brithdate, career, avarage };
+        const usuario = {  nombre_usuario, nombre, apellido, contrasena, codigo, id_rol, id_cargo, id_dependencia };
+        //INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `nombre`, `apellido`, `contrasena`, `codigo`, `id_rol`, `id_cargo`
         const connection= await getConnection();
-        const result = await connection.query("INSERT INTO usuario SET ?",students);
+        const result = await connection.query("INSERT INTO usuario SET ?",usuario);
+        res.json(result);
         res.json({message: "Usuario Added"});
     }catch(error){
         res.status(500);
         res.send(error.message);
     }
 };
+//api para eliminar un usuario
 const deleteUsuario= async (req, res)=> {
     try{
         console.log(req.params);
         const {id} = req.params;
         const connection= await getConnection();
-        const result = await connection.query("DELETE FROM usuario WHERE Id_usuario = ?;",id);    
+        const result = await connection.query("DELETE FROM usuario WHERE id_usuario = ?;",id);    
         res.json(result);
     }catch(error){
         res.status(500);
         res.send(error.message);
     }
 };
+//api para actualizar un usuario
 const updateUsuario= async (req, res)=> {
     try{
         //cambiarlo por los campos de la tabla
-        const {id} = req.params;
-        const { name, lastname, brithdate, career, avarage } = req.body;
-        if(id == undefined || name==undefined || lastname==undefined || brithdate==undefined || career==undefined || avarage==undefined){
+        const {id_usuario} = req.params;
+        const { nombre_usuario, nombre, apellido, contrasena, codigo, id_rol, id_cargo, id_dependencia } = req.body;
+        if(id_usuario == undefined || nombre_usuario==undefined || nombre==undefined || apellido==undefined || contrasena==undefined || codigo==undefined || id_rol==undefined || id_cargo==undefined || id_dependencia==undefined){
             res.status(400).json({message:"Bad Request. Please fill all field."});
         }
-        const student = { id, name, lastname, brithdate, career, avarage };
+        const usuario = { nombre_usuario, nombre, apellido, contrasena, codigo, id_rol, id_cargo, id_dependencia };
         const connection= await getConnection();
-        const result = await connection.query("UPDATE student SET ? WHERE id = ?;",[student, id]);    
+        const result = await connection.query("UPDATE usuario SET ? WHERE id_usuario = ?;",[usuario, id_usuario]);    
         res.json(result);
     }catch(error){
         res.status(500);
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,6 +5,6 @@ const router=Router();
 router.get("/", usuarioController.getUsuarios);
 router.get("/:id_usuario", usuarioController.getUsuario);
 router.post("/", usuarioController.addUsuario);
-router.delete("/:id", usuarioController.deleteUsuario);
-router.put("/:id", usuarioController.updateUsuario);
+router.delete("/:id_usuario", usuarioController.deleteUsuario);
+router.put("/:id_usuario", usuarioController.updateUsuario);
 export default router;
