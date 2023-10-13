2e68b5a6c00dc4d517b89dea7931a94d231c7132
EdvinCanastuj <ecanastujv@miumg.edu.gt>
2023-10-12 22:13:05 -0600

    actualizado 32

--- src/controllers/articulos.controllers.js
+++ src/controllers/articulos.controllers.js
@@ -23,58 +23,58 @@ const getArticulo = async (req, res) => {
 
 const addArticulo = async (req, res) => {
   try {
-  const {
-  id_usuario,
-  codigo,
-  nombre_articulo,
-  no_serie,
-  valor_unitario,
-  valor_baja,
-  observaciones,
-  qr,
-  cantidad
-  } = req.body;
-
-  if (
-  !id_usuario ||
-  !codigo ||
-  !nombre_articulo ||
-  !no_serie ||
-  !valor_unitario ||
-  !valor_baja ||
-  !observaciones ||
-  !qr ||
-  !cantidad
-  ) {
-  res.status(400).json({ message: "Bad Request. Please fill all fields." });
-  return;
-  }
-
-  const connection = await getConnection();
-
-  // Calculamos el valor total en la consulta SQL
-  const sql = `INSERT INTO articulos 
-              (id_usuario, codigo, nombre_articulo, no_serie, valor_unitario, valor_total, valor_baja, observaciones, qr, cantidad) 
-              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-
-  const result = await connection.query(sql, [
+    const {
       id_usuario,
       codigo,
       nombre_articulo,
       no_serie,
       valor_unitario,
-      valor_unitario*cantidad, // Valor unitario * cantidad
       valor_baja,
       observaciones,
       qr,
       cantidad
-      ]);
-  res.json({ message: "Articulo Added" });
-} catch (error) {
-  res.status(500).send(error.message);
-}
-};
+    } = req.body;
 
+    if (
+      !id_usuario ||
+      !codigo ||
+      !nombre_articulo ||
+      !no_serie ||
+      !valor_unitario ||
+      !valor_baja ||
+      !observaciones ||
+      !qr ||
+      !cantidad
+    ) {
+      res.status(400).json({ message: "Bad Request. Please fill all fields." });
+      return;
+    }
+
+    const connection = await getConnection();
+
+    // Convertimos QR 
+    const imageBlob = Buffer.from(qr.split(',')[1], 'base64');
+    // Calculamos el valor total en la consulta SQL
+    const sql = `INSERT INTO articulos (id_usuario, codigo, nombre_articulo, no_serie, valor_unitario, valor_total, valor_baja, observaciones, qr, cantidad) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const result = await connection.query(sql, [
+      id_usuario,
+      codigo,
+      nombre_articulo,
+      no_serie,
+      valor_unitario,
+      valor_unitario * cantidad,
+      valor_baja,
+      observaciones,
+      imageBlob, // Aquí almacenamos la imagen binaria
+      cantidad
+    ]);
+    res.json({ message: "Articulo Added" });
+  } catch (error) {
+    console.error("Error al insertar artículo:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
 const deleteArticulo = async (req, res) => {
   try {
     const { id_articulo } = req.params;
--- src/controllers/usuario.controllers.js
+++ src/controllers/usuario.controllers.js
@@ -74,21 +74,22 @@ const updateUsuario= async (req, res)=> {
         res.send(error.message);
     }
 };
-const loginUsuario= async (req, res)=> {
-    try {
-        const {nombre_usuario, contrasena} = req.body;
-        const connection= await getConnection();
-        const result = await connection.query('SELECT * FROM usuario WHERE nombre_usuario = ? AND contrasena = ?', [nombre_usuario, contrasena]);
-        // res.json(result);
-        if (result.length > 0) {
-            res.json({ message: "Acceso permitido" });
-        } else {
-            res.status(401).json({ message: "Acceso denegado" });
-        }
-    } catch (error) {
-        res.status(500);
-        res.send(error.message);
-}
+const loginUsuario = async (req, res) => {
+try {
+    const { nombre_usuario, contrasena } = req.body;
+    const connection = await getConnection();
+    const result = await connection.query('SELECT * FROM usuario WHERE nombre_usuario = ? AND contrasena = ?', [nombre_usuario, contrasena]);
+
+    if (result.length > 0) {
+        const usuario = result[0];
+        res.json({ message: "Acceso permitido", rol: usuario.id_rol, nombre: usuario.nombre, apellido: usuario.apellido});
+    } else {
+        res.status(401).json({ message: "Acceso denegado" });
+    }
+} catch (error) {
+    res.status(500);
+    res.send(error.message);
+    }
 };
 export const methods={
     getUsuarios,
